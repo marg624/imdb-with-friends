cac803d05669c44f701029921b79bc7c301bebd8
Margarita Miranda <mmiranda@Margaritas-MacBook-Pro.local>
2023-02-23 15:01:22 -0800

    randomly generate game

---
+++ components/start-game.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import arrow from '../public/assets/arrow-icon.png';
+
+type Props = {
+    onClick: React.MouseEventHandler<HTMLButtonElement>
+}
+
+const StartGame = ({
+  onClick,
+}: Props) => {
+  return (
+    <div className="flex justify-center" >
+      <h3 className="text-3xl mb-3 leading-snug">
+        <button onClick={onClick}> Generate new game! </button>
+      </h3>
+    </div>
+  )
+}
+
+export default StartGame
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,9 +8,89 @@ import { CMS_NAME } from '../lib/constants'
 import Post from '../interfaces/post'
 import GuessesBoard from '../components/guesses-board'
 import Game from '../components/game'
+import StartGame from '../components/start-game'
+import gamePairs from '../public/assets/pairs.js';
+import React, {useState} from 'react';
 
 
 export default function Index() {
+
+  const [start, setStart] = useState(null)
+  //useState({"startName": "Natalie Portman", "startUrl": "https://www.imdb.com/name/nm0000204" , "startImageUrl": "https://m.media-amazon.com/images/M/MV5BYzU0ZGRhZWItMGJlNy00YzlkLWIzOWYtNDA2NzlhMDg3YjMwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" });
+  const [end, setEnd] = useState(null)
+  //useState({"endName": "Mila Kunis" , "endUrl": "https://www.imdb.com/name/nm0005109" , "endImageUrl": "https://m.media-amazon.com/images/M/MV5BODQyNTQyNzY4MV5BMl5BanBnXkFtZTcwODg5MDA3MQ@@._V1_FMjpg_UX1000_.jpg" });
+  const [ready, setReady] = useState(false);
+
+  function generateData() {
+    let ran = Math.floor(Math.random() * 6448);
+    let arr = gamePairs.pairs[ran].split(" ");
+    console.log("start: " + arr[0]);
+    console.log("end: " + arr[1]);
+    getActor(arr[0], true);
+    getActor(arr[1], false);
+
+ //   setStart({"startName": "Natalie Portman", "startUrl": "https://www.imdb.com/name/nm0000204" , "startImageUrl": "https://m.media-amazon.com/images/M/MV5BYzU0ZGRhZWItMGJlNy00YzlkLWIzOWYtNDA2NzlhMDg3YjMwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" });
+  //  setEnd({"endName": "Mila Kunis" , "endUrl": "https://www.imdb.com/name/nm0005109" , "endImageUrl": "https://m.media-amazon.com/images/M/MV5BODQyNTQyNzY4MV5BMl5BanBnXkFtZTcwODg5MDA3MQ@@._V1_FMjpg_UX1000_.jpg" });
+    setReady(true);
+
+    console.log(start)
+    console.log(end)
+  }
+
+  function getActor(actor, isStart) {
+    let url = "https://www.imdb.com/name/" + actor; 
+    fetch(url, {
+          crossDomain:true,
+          headers: {'Access-Control-Allow-Origin':'*', 'Access-Control-Allow-Headers':'Origin, Content-Type, Accept'}
+        }).then(response => { 
+          return response.text();
+        }).then(html => { 
+          let n = getName(html);
+          let i = getImage(html);
+          if (n && i) {
+            if (isStart) {
+              setStart({"startName": n, "startUrl": url, "startImageUrl": i});
+            } else {
+              setEnd({"endName": n, "endUrl": url, "endImageUrl": i});
+            }
+          }
+        })
+  }
+
+
+function fetchHtml(url) {
+      return fetch(url, {
+          crossDomain:true,
+          headers: {'Access-Control-Allow-Origin':'*', 'Access-Control-Allow-Headers':'Origin, Content-Type, Accept'}
+        }).then(response => { return response.text();});
+  }
+
+  function getName(html) {
+    // <meta property="og:title" content="Elizabeth Bower - IMDb"/>
+      const parser = new DOMParser();
+      const dom = parser.parseFromString(html, 'text/html');
+      const myMetaElement = dom.querySelector("head meta[property='og:title'][content]");
+      if (myMetaElement) {
+        return (myMetaElement.content.replace(" - IMDb", ""))
+      } else {
+        console.log('No matching element found');
+        return null;
+      }
+    }
+
+    function getImage(html) {
+      //<meta property="og:image" content="https://m.media-amazon.com/images/M/MV5BZDRhMDZiYzEtZWM2YS00ZTU0LWE4Y2QtMTNmODRiYTc0ZmIxXkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_FMjpg_UX1000_.jpg"/>
+      const parser = new DOMParser();
+      const dom = parser.parseFromString(html, 'text/html');
+      const myMetaElement = dom.querySelector("head meta[property='og:image'][content]");
+      if (myMetaElement) {
+        return (myMetaElement.content)
+      } else {
+        console.log('No matching element found');
+        return null;
+      }
+    }
+
   return (
     <>
       <Layout>
@@ -19,18 +99,23 @@ export default function Index() {
         </Head>
         <Container>
           <Intro />
-          <Game startName="Natalie Portman" 
-            startUrl="https://www.imdb.com/name/nm0000204" 
-            startImageUrl="https://m.media-amazon.com/images/M/MV5BYzU0ZGRhZWItMGJlNy00YzlkLWIzOWYtNDA2NzlhMDg3YjMwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" 
-            endName="Mila Kunis" 
-            endUrl="https://www.imdb.com/name/nm0005109" 
-            endImageUrl="https://m.media-amazon.com/images/M/MV5BODQyNTQyNzY4MV5BMl5BanBnXkFtZTcwODg5MDA3MQ@@._V1_FMjpg_UX1000_.jpg" />
-          <GuessesBoard startName="Natalie Portman" 
-            startUrl="https://www.imdb.com/name/nm0000204" 
-            startImageUrl="https://m.media-amazon.com/images/M/MV5BYzU0ZGRhZWItMGJlNy00YzlkLWIzOWYtNDA2NzlhMDg3YjMwXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_FMjpg_UX1000_.jpg" 
-            endName="Mila Kunis" 
-            endUrl="https://www.imdb.com/name/nm0005109" 
-            endImageUrl="https://m.media-amazon.com/images/M/MV5BODQyNTQyNzY4MV5BMl5BanBnXkFtZTcwODg5MDA3MQ@@._V1_FMjpg_UX1000_.jpg"  />
+          { (ready && start && end) &&
+              <span>
+              <Game startName={start["startName"]} 
+                startUrl={start["startUrl"]} 
+                startImageUrl={start["startImageUrl"]} 
+                endName={end["endName"]} 
+                endUrl={end["endUrl"]} 
+                endImageUrl= {end["endImageUrl"]} />
+              <GuessesBoard startName={start["startName"]} 
+                startUrl={start["startUrl"]} 
+                startImageUrl={start["startImageUrl"]} 
+                endName={end["endName"]} 
+                endUrl={end["endUrl"]} 
+                endImageUrl= {end["endImageUrl"]} />
+               </span>
+         }
+         { !ready && <StartGame onClick={generateData} /> }
         </Container>
       </Layout>
     </>
