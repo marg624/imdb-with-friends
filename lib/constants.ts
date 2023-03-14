42ab67779e62ca414d547be99e90d8985bb4f3d5
Margarita Miranda <mmiranda@Margaritas-MacBook-Pro.local>
2023-03-13 18:05:01 -0700

    update meta image

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const EXAMPLE_PATH = 'w/ friends'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
-  'https://github.com/marg624/withfriendscollection/blob/main/public/images/cover.png'
+  'https://github.com/marg624/withfriendscollection/blob/9a1cb9343a78db63cd081ec9f2df650cdef048a9/public/images/cover.png'
