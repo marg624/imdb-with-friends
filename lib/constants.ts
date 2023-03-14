dda3d3a26ab523ca44565df5aa0225fdc5682f07
Margarita Miranda <mmiranda@Margaritas-MacBook-Pro.local>
2023-03-13 18:16:19 -0700

    update meta og:image

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
 export const EXAMPLE_PATH = 'w/ friends'
 export const CMS_NAME = 'Markdown'
-export const HOME_OG_IMAGE_URL =
-  'https://github.com/marg624/withfriendscollection/blob/9a1cb9343a78db63cd081ec9f2df650cdef048a9/public/images/cover.png'
+export const HOME_OG_IMAGE_URL = 'https://wordle-with-friends.s3.us-west-2.amazonaws.com/cover.png'
