e6c49c5e07ef586ec8ae9f882821e58020e70780
Margarita Miranda <mmiranda@Margaritas-MacBook-Pro.local>
2023-03-14 13:20:02 -0700

    #6 add respawn button to get new pair

--- components/game.tsx
+++ components/game.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import arrow from '../public/assets/arrow-icon.png';
+import redo from '../public/assets/redo.png';
 
 type Props = {
   startName: string
@@ -19,9 +20,9 @@ const Game = ({
   endImageUrl,
 }: Props) => {
   return (
-    <div className="flex justify-center" >
+    <div className="flex justify-center " > 
       <h3 className="text-2xl mb-2 leading-snug">
-        <table className="table-fixed shadow-md border-separate p-8" >
+        <table className="table-fixed shadow-md border-separate p-10" >
           <thead>
             <tr>
                <td className="text-center justify-between"> 
@@ -50,7 +51,9 @@ const Game = ({
           </tbody> 
         </table>
       </h3>
-
+      <a href="https://imdb-with-friends.vercel.app/">
+        <img src={redo.src} className="object-contain h-8 w-8 cursor-pointer"  style={{position: 'relative', top: 10, right: 40}}  />
+      </a>  
     </div>
   )
 }
